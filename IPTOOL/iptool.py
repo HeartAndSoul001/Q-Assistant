83911b4e82bef937a52b5f5dccbf1a27a67880ce
wzq <347596212@qq.com>
2023-12-05 22:08:45 +0800

    ip_to_subnetlist:由ip地址计算子网信息表

--- IPTOOL/iptool.py
+++ IPTOOL/iptool.py
@@ -1,6 +1,18 @@
-from netaddr import IPNetwork
+from netaddr import IPNetwork,IPAddress
 
 def ip_to_subnetlist(ip_str):
-    # subnetlist = [prefix_len,mask,iprange,subnet_id,broadcast_ip,hosts]
+    # subnetlist = [prefix_len,mask,host_mask,iprange,subnet_id,broadcast_ip,size]
     subnetlist = []
+    in_ip = IPNetwork(ip_str)
+    for i in range(32,0,-1):
+        ip_network = IPNetwork(ip_str+"/"+str(i))
+        # 掩码
+        mask = str(ip_network.netmask)
+        # 反掩码
+        host_mask = str(ip_network.hostmask)
+        iprange = str(IPAddress(ip_network.first)) + "-" + str(IPAddress(ip_network.last))
+        subnet_id = str(ip_network.network)
+        broadcast_ip = str(ip_network.broadcast)
+        network_size = ip_network.size
+        subnetlist.append([i,mask,host_mask,iprange,subnet_id,broadcast_ip,network_size])
     return subnetlist
